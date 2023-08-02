4d5040deb5ae20b91fb0b357e68a1123bd901d97
auziqni <auziqni@gmail.com>
2023-08-02 11:26:29 +0700

    feat: login with clerk
    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=""
    CLERK_SECRET_KEY=""
    
    NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
    NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL=/dashboard
    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL=/dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Dashboard() {
   return (
     <div>
       <p className="text-lg"> Dashboard (protected)</p>
 
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
       <p>
         <a href="/">landingpage</a>
       </p>
