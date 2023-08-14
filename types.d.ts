7cec7b0a8911d59a378458bc2ffc7a67c0419182
auziqni <auziqni@gmail.com>
2023-08-14 17:54:21 +0700

    refactor: db and types
    changing db to neos posgress
    deleteign unuse type

--- types.d.ts
+++ types.d.ts
@@ -1,29 +1,7 @@
-type Data = {
-  name: string;
-  age: number;
-  // image:string,
-  lat: number;
-  lng: number;
-  date: Date;
-} | null;
-
 type MyLocation = {
   lat: number;
   lng: number;
 } | null;
-
-type Jamaah = {
-  id: number;
-  name: string;
-  gendermale: boolean;
-  age: number;
-  negara: string;
-  rombongan: string;
-  lat: number;
-  lng: number;
-  temp: number;
-  humid: number;
-};
 
 type CardMonitorJamaah = {
   id: number;
