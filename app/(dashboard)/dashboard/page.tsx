9a7807246b4aa17009d4111651d3b1c67a08cdd8
auziqni <auziqni@gmail.com>
2023-08-15 18:52:47 +0700

    refactor: pretty dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,63 +1,12 @@
-// "use client";
-// import { PrismaClient } from "@prisma/client";
-
 import InfoCounter from "@/components/infoCounter";
 import { DialogAdd } from "@/components/dialogAdd";
 import Table from "@/components/table";
-// import { auth } from "@clerk/nextjs";
-// import { useUser } from "@clerk/nextjs";
 
-// const prisma = new PrismaClient();
 import { GetDataAllJamaah, GetDataAdmin } from "@/lib/getdata";
 
-// export const GetDataAllJamaah = async () => {
-//   const res = await prisma.jamaah.findMany({
-//     select: {
-//       id: true,
-//       name: true,
-//       ismale: true,
-//       age: true,
-//       province: true,
-//       group: true,
-//       namaPengurus: true,
-//       lat: true,
-//       lng: true,
-//       temp: true,
-//       humid: true,
-//     },
-//   });
-//   return res;
-// };
-
 export default async function Dashboard() {
-  // const { user } = useUser();
-
   const DataAllJamaah = await GetDataAllJamaah();
   const DataAdmin = await GetDataAdmin();
-
-  // const data = await prisma.userCommunication.findUnique({
-  //   where: { UserId: userId ?? "" },
-  // });
-
-  // if (!data) {
-  //   await prisma.userCommunication.create({
-  //     data: { UserId: userId ?? "" },
-  //   });
-  // }
-
-  // useEffect(() => {
-  //   const newArray = props.filter((obj: AllJamaah) => {
-  //     const distance = calculateDistance(
-  //       myLocation?.lat ?? center.lat,
-  //       myLocation?.lng ?? center.lng,
-  //       obj.Lat ?? center.lat,
-  //       obj.Lng ?? center.lng
-  //     );
-  //     console.log(distance);
-  //     return distance > 7.5;
-  //   });
-  //   myLocation ? setjamaahOutranged(newArray) : "";
-  // }, []);
 
   return (
     <div className="p-10">
