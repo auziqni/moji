8c4c64ca39ddfdb790c7f536806fec64ea6dfe4c
auziqni <65333301+auziqni@users.noreply.github.com>
2023-11-13 13:27:16 +0700

    pindah laptop

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "MoJi - Monitoring haji",
@@ -18,7 +16,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body >
           {children}
           <Toaster />
         </body>
